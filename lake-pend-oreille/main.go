2afc069bfc5db0c7794abf9553671a85cf0af5b5
gevuong <gevuong18@gmail.com>
2018-07-19 04:14:59 -0500

    calculate mean and medium after parsing lake-pend-oreille data change author to personal github

--- lake-pend-oreille/main.go
+++ lake-pend-oreille/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/csv"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
+	"time"
 )
 
 // In Go, slices are built on top of arrays, and slices are dynamic, meaning they
@@ -47,21 +49,33 @@ func main() {
 
 	// loop each row
 	for i, row := range rows {
-		// fmt.Println(i, row)
+		airTemp := row[1]
+		baroPress := row[2]
+		windSpeed := row[7]
+		if i >= 1 && i < 5 {
+			fmt.Println(i, row)
+		}
 		if i == 1 {
 			// print type using %T format verb for desired values: air_temp,
 			// barometric_press, and wind_speed fields.
-			fmt.Printf("%T %T %T\n", row[1], row[2], row[7])
-			fmt.Println(row[1], row[2], row[7])
+			fmt.Printf("%T %T %T\n", airTemp, baroPress, windSpeed)
+			fmt.Println(airTemp, baroPress, windSpeed)
 			// break
 		}
 	}
 
+	// calculate start time to process mean for 3 fields.
+	start := time.Now()
 	fmt.Println("total records: ", len(rows)-1) // -1 due to zero-based index
-	fmt.Println("avg air temp: ", mean(rows, 1))
-	fmt.Println("avg baro press: ", mean(rows, 2))
-	fmt.Println("avg wind speed: ", mean(rows, 7))
+	fmt.Println("avg air temp: ", mean(rows, 1), median(rows, 1))
+	fmt.Println("avg baro press: ", mean(rows, 2), median(rows, 2))
+	fmt.Println("avg wind speed: ", mean(rows, 7), median(rows, 7))
+	end := time.Now()
+	// Sub returns the duration between two times.
+	delta := end.Sub(start)
+	fmt.Println("Abstracted mean performance: ", delta)
 
+	median(rows, 1)
 }
 
 func mean(rows [][]string, idx int) float64 {
@@ -69,6 +83,7 @@ func mean(rows [][]string, idx int) float64 {
 
 	// loop each row
 	for i, row := range rows {
+		// skip header row
 		if i != 0 {
 			// ParseFloat converts string data from indexing row, to a float64
 			// data type, which will later be used for calculations.
@@ -79,4 +94,42 @@ func mean(rows [][]string, idx int) float64 {
 		}
 	}
 	return total / counter // or: total / float64(len(rows)-1)
+}
+
+func median(rows [][]string, idx int) float64 {
+	// holds data which will be sorted
+	var sorted []float64
+
+	for i, row := range rows {
+		// skip header row
+		if i != 0 {
+			val, _ := strconv.ParseFloat(row[idx], 64)
+			sorted = append(sorted, val)
+		}
+	}
+
+	// fmt.Println("\nBEFORE SORTING")
+	// for i, val := range sorted {
+	// 	if i < 15 {
+	// 		fmt.Printf("%f\n", val)
+	// 	}
+	// }
+	// sort []float64 in increasing order
+	sort.Float64s(sorted)
+
+	// fmt.Println("\nAFTER SORTING")
+	// for i, val := range sorted {
+	// 	if i < 15 {
+	// 		fmt.Printf("%f\n", val)
+	// 	}
+	// }
+
+	if len(sorted)%2 == 0 { // meaning length is even
+		mid := len(sorted) / 2
+		higher := sorted[mid]
+		lower := sorted[mid-1]
+		return (higher + lower) / 2
+	}
+	mid := len(sorted) / 2
+	return sorted[mid]
 }
