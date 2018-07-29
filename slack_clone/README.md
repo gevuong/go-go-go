d709906ded3c20dc2efcc1bfea0c7ec0b510a1a9
gevuong <gevuong18@gmail.com>
2018-07-29 16:40:05 -0500

    test websocket and update README

--- slack_clone/README.md
+++ slack_clone/README.md
@@ -4,4 +4,4 @@
 - We will serve frontend HTML and JS to dedicated servers such as Nginx, or CDN if appropriate. Leave normal HTTP traffic to Nginx or CDN.
 - Websockets allow connections to any host. There are no browser-based same origin policy. The origin policy is left to the server to decide. In other words, it's up to the server to determine if it's okay for the web browser to make a websocket connection.
 
-- To test websocket communication: `http://websocket.org/echo.html`. Input whichever localhost you are using (i.e. `ws://localhost:4000`), then a message to send.
+- To test websocket communication: `http://websocket.org/echo.html`. Input whichever localhost you are using (i.e. `ws://localhost:4000`), click "CONNECT", and make sure log says "CONNECTED". Then input a message to send. And you'll see the message sent and echoed back to us. You will also see the message logged in the terminal.
--- slack_clone/server.go
+++ slack_clone/server.go
@@ -11,37 +11,44 @@ func main() {
 	// pass in URL pattern and handler fcn to execute
 	http.HandleFunc("/", handler)
 	// waits until port is connected to or app is blocked
+	fmt.Println("Listening on port 4000...")
 	http.ListenAndServe(":4000", nil)
-
 }
 
 // upgrader "upgrades" the HTTP server connection to the WebSocket protocol.
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	// returning true allows WebSocket connection from any origin
+	// modern web browsers allow web socket connections to any host. We want a
+	// no browser based same-origin policy. Gorilla websockets
+	// default behavior forces a same origin policy. Lets leave origin policy for
+	// the server to decide. The server will determine if it's okay for the browser
+	// to make a websocket connection.
+	//
+	// Assign a new fcn to CheckOrigin and return TRUE indicating we'll allow
+	// connections from any origin.
 	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	// pass in a writer and string we'd like written as a response
 	// fmt.Fprintf(w, "this is how we create a basic HTTP web server in Go")
-
-	var socket, err = upgrader.Upgrade(w, r, nil)
+	socket, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// if upgrade is successful, read and write message
+	// if Upgrade is successful, read and write message in websocket
 	for {
+		// ReadMessage is a fcn that blocks until a message is received.
 		msgType, msg, err := socket.ReadMessage()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println(string(msg))
-		// write msg back to the client
+		fmt.Println(string(msg), msgType)
+		// echo the msg back to the client
 		// an 'if' condition allows you to specify statement before conditional
 		if err := socket.WriteMessage(msgType, msg); err != nil {
 			fmt.Println(err)
